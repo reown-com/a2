b81602730a1fe20b40132af9a3b167fd9c87fd23
Sergey Tkachenko <seriy.tkachenko@gmail.com>
2016-03-03 01:11:21 +0200

    Added service.rs and responce.rs files

---
+++ src/response.rs
@@ -0,0 +1,3 @@
+pub struct Response {
+    pub status: u32,
+}
---
+++ src/service.rs
@@ -0,0 +1,50 @@
+use rustc_serialize::json::ToJson;
+use std::path::{Path};
+use hyper::Client;
+use hyper::http::h2::Http2Protocol;
+use hyper::net::{HttpsConnector, Openssl};
+use ::payload::*;
+use ::device_token::*;
+use ::response::*;
+
+static DEVELOPMENT: &'static str = "https://api.development.push.apple.com";
+static PRODUCTION:  &'static str = "https://api.push.apple.com";
+
+// Request headers
+header! { (APNSId, "apns-id") => [String] }
+header! { (APNSExpiration, "apns-expiration") => [String] }
+header! { (APNSPriority, "apns-priority") => [String] }
+header! { (APNSTopic, "apns-topic") => [String] }
+
+pub struct Service {
+    pub client: Client,
+    pub path: String,
+}
+
+impl Service {
+    pub fn new(sandbox: bool, certificate_path: &str, private_key_path: &str) -> Service {
+        let ssl = Openssl::with_cert_and_key(Path::new(certificate_path), Path::new(private_key_path)).unwrap();
+        let ssl_connector = HttpsConnector::new(ssl);
+        let client = Client::with_protocol(Http2Protocol::with_connector(ssl_connector));
+        let path = if sandbox {
+            format!("{}{}", DEVELOPMENT, "/3/device/")
+        } else {
+            format!("{}{}", PRODUCTION, "/3/device/")
+        };
+        Service {client: client, path: path}
+    }
+
+    pub fn push(&self, payload: Payload, token: DeviceToken) -> Response {
+        let url = format!("{}{}", self.path, token.token);
+        let url_str: &str = &url;   // .as_str() waiting on RFC revision (see issue #27729)
+        let pay = payload.to_json().to_string();
+        let pay_str: &str = &pay;
+        println!("{:?}", pay_str);
+        let res = self.client.post(url_str)
+            .body(pay_str)
+            .send().unwrap();
+        println!("{:?}", res);
+
+        Response {status: 200}
+    }
+}
