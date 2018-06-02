32df8d3cc9914718e1bf62e6a8f48e2402835664
Julius de Bruijn <julius@nauk.io>
2018-06-02 10:23:46 +0200

    Fix key location in docs script

--- .travis/docs.sh
+++ .travis/docs.sh
@@ -39,7 +39,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../a2_travis.enc -out a2_travis -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./a2_travis.enc -out a2_travis -d
 chmod 600 a2_travis
 eval `ssh-agent -s`
 ssh-add a2_travis
